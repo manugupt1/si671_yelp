65433cc73b9c0785879db2f6b98c71a698e4e927
Manu Gupta <manugupt1@gmail.com>
2015-11-03 18:57:25 -0500

    -- Adding category counts
    TODO
    TUrn into mutual information, revise slides and implement it.
    Take two categories and form a list of reviews

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 __author__ = 'Manu'
 
-import simplejson as json
 from collections import Counter
+from itertools import combinations
+
+import simplejson as json
 
 ''' Changing filenames to variables"'''
 
@@ -11,8 +13,10 @@ review = "yelp_academic_dataset_review.json"
 tip  = "yelp_academic_dataset_tip.json"
 user  = "yelp_academic_dataset_user.json"
 
+
 ''' categoryCount keeps track of number of businesses in each category '''
 categoryCount = Counter()
+categoryRelationships = Counter()
 
 
 with open(business,"r") as bizFile,open("categories","w") as output:
@@ -20,18 +24,46 @@ with open(business,"r") as bizFile,open("categories","w") as output:
         string = json.loads(line)
         categories = string['categories']
         output.write(str(categories)+"\n")
-        for category in categories:
-            categoryCount.update((category,))
-#        break
+        x = combinations(categories, 2)
+        for i in x:
+            temp = frozenset(i)
+            categoryRelationships.update((temp,))
+# for category in categories:
+#             categoryCount.update((category,))
+# #        break
 
-#Sort the counter, it is a list now instead of a counter
-categoryCount = categoryCount.most_common()
-print (type(categoryCount))
 
-with open("categoryCountFile","w") as output:
-    for item in categoryCount:
-        val =  item[0] + "\t" + str(item[1]) + "\n"
-        output.write (val)
-
+# #Sort the counter, it is a list now instead of a counter
+# categoryCount = categoryCount.most_common()
 
 #
+# with open("categoryCountFile","w") as output:
+#     for item in categoryCount:
+#         val =  item[0] + "\t" + str(item[1]) + "\n"
+#         output.write (val)
+
+
+# Finding out the number of businesses in every cities
+# city = Counter()
+# with open(business,"r") as file,open("city","w") as output:
+#     for line in file:
+#         string = json.loads(line)
+#         string = string['city'].strip()
+#         city.update((string,))
+#
+#
+# city = city.most_common()
+#
+# with open("city","w") as output:
+#     for item in city:
+#         val =  item[0] + "\t" + str(item[1]) + "\n"
+#         output.write (val)
+#
+
+
+# Relationships between two categories
+categoryRelationships = categoryRelationships.most_common()
+with open("categoryRelationships", "w") as output:
+    for item in categoryRelationships:
+        val = str(item[0]) + "\t" + str(item[1]) + "\n"
+        output.write(val)
